49ba99d2a1290d9757db873f82ebb0a82acc1c61
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-20 11:47:30 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,13 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
-        int VariableSix = 6;
-        int variableSeven = 7;
-        int VariableEight = 8;
+        int morrow = 0;
+        int money = 10;
+        int hundred = 100;
+        int back = 1000;
+        int underscore = 10000;
+        int sharp = 100000;
+        int star = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
